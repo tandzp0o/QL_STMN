39cb06acd2cf18053b5c8c17c397ec300613f6ce
LunChannn <blue26011994@gmail.com>
2023-12-31 09:48:38 +0700

    update form NhanVien

--- QL_STMN/QL_STMN/KetNoiDB.cs
+++ QL_STMN/QL_STMN/KetNoiDB.cs
@@ -8,6 +8,6 @@ namespace QL_STMN
 {
     public class KetNoiDB
     {
-        public static string strconn = "Data Source=TONDZP;Initial Catalog=SieuThiMini;Integrated Security=True";
+        public static string strconn = "Data Source=LAPTOP-DV021U52\\LUNCHAN;Initial Catalog=SieuThiMini;Integrated Security=True";
     }
 }
